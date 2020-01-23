64f6ca38917b47faa5053eef604e422649d51484
Daniel Succar <danielsuccar@icloud.com>
2020-01-23 16:14:55 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,9 +3,17 @@ require 'directors_database'
 require 'pp'
 
 def pretty_print_nds(nds)
-  pp nds 
+  pp nds
   nil
 end
 
 def print_first_directors_movie_titles
+  require 'pp'
+  print_first_directors_movie_titles = [
+    'jaws','Close Encounters of the Third Kind','Raiders of the Lost Ark', 'E.T. the Extra-terrestrial', 'Schindler\'s List',
+    'Lincoln'
+  ]
+
+  pp print_first_directors_movie_titles
 end
+Jaws\nClose Encounters of the Third Kind\nRaiders of the Lost Ark\nE.T. the Extra-terrestrial\nSchindler's List\nLincoln\n
